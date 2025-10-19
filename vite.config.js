a7125cf10a979643473edd3ae558d606ab6efbae
Yusri Nelson <yusrinelson17@gmail.com>
2025-10-19 16:11:47 +0200

    updated for SEO

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,18 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [
     vue(),
-    vueDevTools(),
+    // Only include Vue DevTools in development mode
+    ...(mode === 'development' ? [vueDevTools()] : [])
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
-})
+}))
